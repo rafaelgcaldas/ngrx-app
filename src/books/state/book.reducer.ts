0664e59f7049f5184ce23a5755006bc21a47a540
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-26 15:18:07 -0300

    feat: add book reducer

--- src/books/state/book.reducer.ts
+++ src/books/state/book.reducer.ts
@@ -1,8 +1,10 @@
+import { createReducer, on } from "@ngrx/store";
 import type { Book } from "../book.model";
+import { BookActions } from "./book.actions";
 
 enum BookStatus {
   loading = "loading",
-  pendings = "pending",
+  pending = "pending",
   error = "error",
   success = "success"
 }
@@ -12,3 +14,31 @@ export interface BookState {
   error: string | null
   status: BookStatus
 }
+
+const initialState: BookState = {
+  error: null,
+  status: BookStatus.pending,
+  books: []
+}
+
+export const bookReducer = createReducer (
+  initialState,
+  on(BookActions.loadBooks, (currentState) => {
+    return {
+      ...currentState,
+      status: BookStatus.success,
+      books: initalBooks
+    }
+  })
+)
+
+const initalBooks = [
+  {
+    id: 1,
+    name: 'John Smith',
+  },
+  {
+    id: 2,
+    name: 'Jkins Smith',
+  },
+]
