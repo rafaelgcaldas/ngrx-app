a755eaf42fc61b1f95b556c8bf758006db43a882
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-26 15:54:49 -0300

    feat: add effect provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,14 @@ import { provideStore } from '@ngrx/store';
 import { BookService } from '../books/book.service';
 import { routes } from './app.routes';
 import { appReducers } from './state/app.reducers';
+import { provideEffects } from '@ngrx/effects';
+import { getBookEffect } from '../books/state/book.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    BookService, 
-    provideStore(appReducers)
-  ]
+    provideRouter(routes),
+    BookService,
+    provideStore(appReducers),
+    provideEffects({ getBookEffect })
+]
 };
