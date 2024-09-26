0e418c3f4dac04f18e2e87bf232956aaeb73e89d
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-26 14:55:08 -0300

    chore: ngrx config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { BookService } from '../books/book.service';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), BookService]
+  providers: [
+    provideRouter(routes), 
+    BookService, 
+    provideStore()]
 };
