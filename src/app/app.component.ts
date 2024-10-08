a37ef33a050c73cf2fdbc0bb7ba69c69719b382a
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-08 15:38:26 -0300

    feat: add book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, type OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 import { Store } from '@ngrx/store';
+
 import { BookService } from '../books/book.service';
 import { bookActions } from '../books/state/book.actions';
-import { bookSelector } from '../books/state/livro.selectors';
+import { bookSelector } from '../books/state/book.selectors';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'ngrx-app';
+  title = 'ngrx-app'
+  inputBook = ''
   
   bookService = inject(BookService);
   store = inject(Store)
@@ -23,5 +27,12 @@ export class AppComponent implements OnInit {
   
   ngOnInit(): void {
     this.store.dispatch(bookActions.loadBooks())
+  }
+
+  addBook() {
+    this.store.dispatch(bookActions.addBook({
+      id: 3,
+      name: this.inputBook
+    }))
   }
 }
